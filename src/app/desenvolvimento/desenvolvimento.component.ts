414280dc75bc402f4b86609a2ac097d484b25d99
LUIS MIGUEL DE JESUS LOBO <021.909446@universidadesenaicimatec.edu.br>
2025-11-24 15:26:11 -0300

    LOOP icons complet

--- src/app/desenvolvimento/desenvolvimento.component.ts
+++ src/app/desenvolvimento/desenvolvimento.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-desenvolvimento',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './desenvolvimento.component.css'
 })
 export class DesenvolvimentoComponent {
+
+
 
 }
